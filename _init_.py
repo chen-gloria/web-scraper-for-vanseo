c0a27abab1d4fb3c84892810820f9881057f2ed9
Rouyu Chen <gloria.rouyu@gmail.com>
2021-02-02 17:45:01 +1100

    根据Q的爬虫修改

--- _init_.py
+++ _init_.py
@@ -1,33 +1,17 @@
-import bs4
 import requests
-from bs4 import BeautifulSoup
+
+url_main = 'https://www.poliform.it/'
+url_product = 'https://www.poliform.it/en-us/products'
+url_items = 'https://www.poliform.it/en-us/products/preview-2021/le-club'
 
 def getHTMLText(url):
-    try:
-        r = requests.get(url, timeout=30)
-        r.raise_for_status() # generate error information
-        r.encoding = r.apparent_encoding # could be revised to enhance the speed
-        return r.content # return the HTML to other parts of the programmme
-    except:
-        return ""
-
-def fillUnivKust(ulist, html):
-    soup = BeautifulSoup(html, "html.parser")
-    for a in soup.find('li').children:
-        if isinstance(a, bs4.element.Tag): # avoid String type's data
-            aaa = a('div') # There are only 2 divs here in this case
-            ulist.append([aaa[0].string]) # aaa[0] -> Product's name
-
-def printUnivList(ulist, num):
-    for i in range(num):
-        u = ulist[i] # u already have
-        print(u[i]) # print the ith product's name
+    data = {'idCategory' : 'new'}
+    r = requests.post(url, data, timeout=30)
+    print(r.content)
+    return r.content
 
 def main():
-    uinfo = []
-    url = 'https://www.cattelanitalia.com/en/products?c=new'
-    html = getHTMLText(url)
-    fillUnivKust(uinfo, html)
-    printUnivList(uinfo, 25)
+    url = 'https://www.cattelanitalia.com/en/products/productList/0/'
+    getHTMLText(url)
 
 main()
